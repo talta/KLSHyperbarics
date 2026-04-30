4f5660c0ca2b092d6c55ec0fcc589f2e3367c60a
TALTA <t.taltallc@gmail.com>
2026-04-30 16:21:37 -0400

    Updated text.
    changes to the menu prices, the learn more and about us. and contact us.   Does not include a working map.   Future feature.

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,4 +398,13 @@
 						$main._show(location.hash.substr(1), true);
 					});
 
+
+		//Map functionality.
+			var map = L.map('map').setView([51.505, -0.09], 13);
+
+			L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 })(jQuery);
